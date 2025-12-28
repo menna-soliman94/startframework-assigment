17b8a18168b4e8580c73109d0f1e6f0792f2fb64
menna-soliman94 <menna.soliman94@gmail.com>
2025-12-28 02:07:39 +0200

    fix routing

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
+import avatar from "../../assets/avataaars.svg";
 
 export default function Home() {
   useEffect(() => {
@@ -10,7 +11,7 @@ export default function Home() {
     <>
       <div className="home text-white py-4">
         <div className="container d-flex flex-column justify-content-center align-items-center">
-          <img className="w-25" src="/src/assets/avataaars.svg" alt="Avatar" />
+          <img className="w-25" src={avatar} alt="Avatar" />
           <h1 className="fw-bold mt-2">START FRAMEWORK</h1>
           <div className="d-flex justify-content-center align-items-center my-3">
             <div className="line bg-white"></div>
